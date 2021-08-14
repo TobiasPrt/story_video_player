4e5c00103c587a6019d8c73cb478d8d84da3ddf4
Tobias Pörtner <t.poertner@outlook.com>
2021-08-14 08:43:04 +0200

    Added example project

---
+++ example/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Example());
+
+class Example extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Story Video Player Example',
+      home: Scaffold(),
+    );
+  }
+}
